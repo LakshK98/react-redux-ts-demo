12ecfd40f4130b3f2bb5b9494229046fb686d7d2
Laksh <lakshkotian98@gmail.com>
2024-04-22 14:02:57 -0400

    fixed logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './Header.module.css';
+import logo from '../../stackline_logo.svg';
 
 const Header: React.FC = () => {
   return (
     <div className={styles.header}>
       <div className={styles.logo}>
-        <img src="/stackline_logo.svg" alt="Logo" />
+        <img src={logo} alt="Logo" />
       </div>
     </div>
   );
